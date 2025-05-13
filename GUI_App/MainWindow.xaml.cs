e7fe96ca2d834f156f314d9ba3117713c0ea0279
Mateusz0W <153832537+Mateusz0W@users.noreply.github.com>
2025-05-13 18:47:04 +0200

    first GUI implementation

---
+++ GUI_App/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+
+
+namespace FamilyTreeGraph
+{
+    public partial class MainWindow : Window
+    {
+        private const double BoxWidth = 80;
+        private const double BoxHeight = 40;
+        private const double HorizontalSpacing = 30;
+        private const double VerticalSpacing = 80;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            string connectionString = "Server=.;Database=Project;TrustServerCertificate=True;Integrated Security=True;";
+            var service = new HierarchyService(connectionString);
+            Dictionary<string,Person> tree = service.readTree();
+            Person root = tree.Values.First(p => !tree.Values.Any(x => x.getChildrens().Contains(p)));
+            DrawTree(root, 0, 20, out _);
+        }
+
+        // Rekursyjne rysowanie drzewa
+        private double DrawTree(Person person, double x, double y, out double centerX)
+        {
+            if (person.getChildrens().Count == 0)
+            {
+                DrawPersonBox(person.getName(), x, y);
+                centerX = x + BoxWidth / 2;
+                return x + BoxWidth;
+            }
+
+            double currentX = x;
+            List<double> childCenters = new();
+
+            foreach (var child in person.getChildrens())
+            {
+                double subtreeX = DrawTree(child, currentX, y + BoxHeight + VerticalSpacing, out double childCenter);
+                childCenters.Add(childCenter);
+                currentX = subtreeX + HorizontalSpacing;
+            }
+
+            centerX = (childCenters[0] + childCenters[^1]) / 2;
+            DrawPersonBox(person.getName(), centerX - BoxWidth / 2, y);
+
+            // Rysowanie linii do dzieci
+            foreach (var childCenter in childCenters)
+            {
+                DrawLine(centerX, y + BoxHeight, childCenter, y + BoxHeight + VerticalSpacing);
+            }
+
+            return currentX;
+        }
+
+        private void DrawPersonBox(string name, double x, double y)
+        {
+            Border border = new Border
+            {
+                Width = BoxWidth,
+                Height = BoxHeight,
+                BorderBrush = Brushes.Black,
+                BorderThickness = new Thickness(1),
+                Background = Brushes.LightYellow,
+                Child = new TextBlock
+                {
+                    Text = name,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center
+                }
+            };
+            Canvas.SetLeft(border, x);
+            Canvas.SetTop(border, y);
+            TreeCanvas.Children.Add(border);
+        }
+
+        private void DrawLine(double x1, double y1, double x2, double y2)
+        {
+            Line line = new Line
+            {
+                X1 = x1,
+                Y1 = y1,
+                X2 = x2,
+                Y2 = y2,
+                Stroke = Brushes.Black,
+                StrokeThickness = 1
+            };
+            TreeCanvas.Children.Add(line);
+        }
+
+    }
+}
